21567d71f974e01ba9d2a37deb6d8579d682f928
Mamuddeen87 <bellog.ug@atbu.edu.ng>
2025-11-14 12:03:27 -0800

    pattern made with while

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,3 +1,11 @@
-size = int(input("Enter the size of the pattern: "))
-for _ in range(size):
-    print("*" * size)
+pattern_size = int(input("Enter the size of the pattern: "))
+
+i = 0
+while i < pattern_size:
+    j = 0
+    while j < pattern_size:
+        print("*", end="")
+        j += 1
+    print()  # new line after each row
+    i += 1
+
